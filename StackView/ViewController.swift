58a1f16e62649fa0174d8799c79b7ea297858498
javidpoornasir <javidp.sf@gmail.com>
2019-03-18 19:34:19 -0700

    Initial Commit

---
+++ StackView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackView
+//
+//  Created by Javid Poornasir on 3/18/19.
+//  Copyright © 2019 jp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
